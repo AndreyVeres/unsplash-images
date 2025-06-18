fceecd9072e49258ea5c49d957285f63cc7aebf9
Andrey Veres <spacecmetanka@gmail.com>
2025-06-18 11:49:44 +0300

    fix: handler

--- api/token.js
+++ api/token.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import qs from 'qs';
 
-export default function handler(req, res) {
+export default async function token(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
@@ -26,15 +26,13 @@ export default function handler(req, res) {
       grant_type: 'authorization_code',
     });
 
-    axios
-      .post(`${api_auth_base}token`, data, {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-      })
-      .then(result => {
-        res.status(200).json({ token: result.data.access_token });
-      });
+    const response = await axios.post(`${api_auth_base}token`, data, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+
+    res.status(200).json({ token: response.data.access_token });
   } catch (error) {
     console.error('Error fetching token:', error.response ? error.response.data : error.message);
     res.status(500).json({ message: 'Internal Server Error' });

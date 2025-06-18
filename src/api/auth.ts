d6118c5046f305c798d26c5d362ff9a80d0c2485
Andrey Veres <spacecmetanka@gmail.com>
2025-06-18 12:24:08 +0300

    fix: useless code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -5,16 +5,6 @@ export const axiosAuth = axios.create({
 });
 
 export const getCode = () => {
-  // const clientId = import.meta.env.VITE_CLIENT_ID;
-  // const redirectUri = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI);
-
-  // const scope = 'public+read_user+write_likes';
-  // const authUrl = `${
-  //   import.meta.env.VITE_API_AUTH_BASEURL
-  // }authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
-
-  // window.location.assign(authUrl);
-
   return new Promise<string>(res => {
     fetch('/api/code', {
       method: 'GET',

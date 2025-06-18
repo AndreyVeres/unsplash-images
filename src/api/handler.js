0e445535c94fc83d60d78af30200d1756a9d9774
Andrey Veres <spacecmetanka@gmail.com>
2025-06-18 10:39:45 +0300

    fix: handler

--- src/api/handler.js
+++ src/api/handler.js
@@ -1,14 +1,10 @@
-
 import axios from 'axios';
 import qs from 'qs';
 
-
-export default async function handler(req, res) {
-
+export default function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
-
 
   const client_id = process.env.CLIENT_ID;
   const client_secret = process.env.CLIENT_SECRET;
@@ -30,16 +26,16 @@ export default async function handler(req, res) {
       grant_type: 'authorization_code',
     });
 
-    const response = await axios.post(`${api_auth_base}/token`, data, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    });
-
-    // Отправляем на фронтенд только то, что ему нужно (токен)
-    res.status(200).json({ token: response.data.access_token });
+    const response = axios
+      .post(`${api_auth_base}/token`, data, {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .then(result => {
+        res.status(200).json({ token: result.data.access_token });
+      });
   } catch (error) {
-    // Логируем ошибку на сервере для отладки
     console.error('Error fetching token:', error.response ? error.response.data : error.message);
     res.status(500).json({ message: 'Internal Server Error' });
   }
